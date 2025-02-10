d9b1cc0855655b0d06bb072879c78471978d96ee
Dustin Teaches Tech <dustin7336@gmail.com>
2025-02-10 01:37:32 -0600

    UPDATED FUNCTIONS 2-10-25 1:37pm

--- infinteger.pas
+++ infinteger.pas
@@ -172,7 +172,64 @@ begin
   end;
 end;
 
+procedure SaveToFile(filename: string; output: AnsiString);
+var
+    outputFile: Text;
+begin
+    Assign(outputFile, filename); // Link file variable to the actual file
+    Rewrite(outputFile); // Open file for writing (this erases previous content)
+
+    if IOResult <> 0 then 
+    begin
+        Writeln('Error opening file: ', filename);
+        Exit;
+    end;
+
+    // Write the gathered output string to the file
+    writeln(outputFile, output);
+
+    Close(outputFile); // Close the file
+
+    if IOResult <> 0 then 
+    begin
+        Writeln('Error closing file: ', filename);
+    end;
+
+    Writeln('Output saved to file: ', filename);
+end;
+
+function GetNumberAsString(num: PNode): AnsiString;
+var
+  current: PNode;
+  result: string;
+  leadingZero: boolean;
+begin
+  result := '';
+  current := num;
+  leadingZero := true; // Flag to skip leading zeros
+
+  while current <> nil do
+  begin
+    // Skip leading zeros
+    if leadingZero and (current^.data = 0) then
+    begin
+      current := current^.next; // Move to the next node
+      continue;  // Skip printing the zero
+    end;
+
+    leadingZero := false; // After the first non-zero digit, we print all digits
+    result := result + IntToStr(current^.data); // Append to result string
+    current := current^.next; // Move to the next node
+  end;
+
+  GetNumberAsString := result; // Return the number as a string
+end;
+
+
+
 // Main program logic
+var
+  output: AnsiString;
 begin
   Randomize;
   head1 := nil;
@@ -188,23 +245,42 @@ begin
   writeln('Generating random number 2...');
   GenerateRandomNumber(head2, tail2);  // Random length (1-256 digits)
   
-  // Print the original numbers
-  writeln('Number 1:');
-  PrintNumber(head1);
-  
-  writeln('Number 2:');
-  PrintNumber(head2);
+  // Gather the output for the first number
+  output := 'Number 1: ';
+  output := output + GetNumberAsString(head1) + sLineBreak;
+
+  // Gather the output for the second number
+  output := output + 'Number 2: ';
+  output := output + GetNumberAsString(head2) + sLineBreak;
 
   // Add the numbers and store the result in the sum linked list
   AddNumbers(tail1, tail2, headSum, tailSum);
 
-  // Print the sum
-  writeln('Sum:');
-  PrintNumber(headSum);
+  // Gather the output for the sum
+  output := output + 'Sum: ';
+  output := output + GetNumberAsString(headSum) + sLineBreak;
 
   // Test if the sum is prime
   if IsPrimeLinkedList(headSum) then
-    writeln('The sum is prime.')
+    output := output + 'The sum is prime.' + sLineBreak
   else
-    writeln('The sum is not prime.');
+    output := output + 'The sum is not prime.' + sLineBreak;
+
+
+// writeln('Number 1: ', GetNumberAsString(head1));
+// writeln('Number 2: ', GetNumberAsString(head2));
+// writeln('Sum: ', GetNumberAsString(headSum));
+
+// if IsPrimeLinkedList(headSum) then
+//   writeln('The sum is prime.')
+// else
+//   writeln('The sum is not prime.');
+
+
+
+writeln(output);  // Check the output before saving to the file
+
+
+  // Save all output to the file
+  SaveToFile('infinteger.txt', output);
 end.
